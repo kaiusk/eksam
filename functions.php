0252a9c7a70f26d8f42501903bd74dd400cad77d
Kaius Karon <kaiusk@tlu.ee>
2022-06-09 15:39:59 +0300

    style

--- functions.php
+++ functions.php
@@ -38,67 +38,6 @@ function my_style_enqueue_styles() {
 }
 add_action('wp_enqueue_scripts', 'my_style_enqueue_styles');
 
-
-/* Custom Post Type Start */
-/*
-function create_posttype() {
-    register_post_type( 'employee',
-// CPT Options
-        array(
-            'labels' => array(
-                'name' => __( 'employee' ),
-                'singular_name' => __( 'employee' )
-            ),
-            'public' => true,
-            'has_archive' => false,
-            'rewrite' => array('slug' => 'employee'),
-        )
-    );
-}
-// Hooking up our function to theme setup
-add_action( 'init', 'create_posttype' );
-
-function cw_post_type_news() {
-    $supports = array(
-        'title', // post title
-        'editor', // post content
-        'author', // post author
-        'thumbnail', // featured images
-        'excerpt', // post excerpt
-        'custom-fields', // custom fields
-        'comments', // post comments
-        'revisions', // post revisions
-        'post-formats', // post formats
-    );
-    $labels = array(
-        'name' => _x('news', 'plural'),
-        'singular_name' => _x('news', 'singular'),
-        'menu_name' => _x('news', 'admin menu'),
-        'name_admin_bar' => _x('news', 'admin bar'),
-        'add_new' => _x('Add New', 'add new'),
-        'add_new_item' => __('Add New news'),
-        'new_item' => __('New news'),
-        'edit_item' => __('Edit news'),
-        'view_item' => __('View news'),
-        'all_items' => __('All news'),
-        'search_items' => __('Search news'),
-        'not_found' => __('No news found.'),
-    );
-    $args = array(
-        'supports' => $supports,
-        'labels' => $labels,
-        'public' => true,
-        'query_var' => true,
-        'rewrite' => array('slug' => 'news'),
-        'has_archive' => true,
-        'hierarchical' => false,
-    );
-    register_post_type('news', $args);
-}
-add_action('init', 'cw_post_type_news');
-*/
-/* Custom Post Type End */
-
 function create_copyright(): void {
     $all_posts = get_posts(
         'post_status=publish&order=ASC');
